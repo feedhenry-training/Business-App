6fce3ae01b3f9c571503a6e63cc8521a43762937
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-22 18:05:19 +0000

    comment for mortgage

--- cloud/mortgage.js
+++ cloud/mortgage.js
@@ -3,12 +3,16 @@
  * Use this Web service to figure out your monthly mortgage payment.
  */
 var mortgage = {
+	//SOAP API URL
 	SOAPUrl : "http://www.webservicex.net/mortgage.asmx",
 	/**
 	 * Calc mortgaeg based on user input.
 	 * Tutorial: How to wrap SOAP message and unwrap SOAP response.
 	 */
 	getMortgage : function(years, interest,loanAmount,tax,insurance) {
+		/**
+		 * Since SOAP calls are wrapped HTTP calls, in Javascript we have to wrap SOAP envelope manually or using a SOAP library in Javascript
+		 */
 		var xmlContent = '<?xml version="1.0" encoding="utf-8"?>' + 
 		'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' + 
 		' <soap:Body>' + 
@@ -22,7 +26,9 @@ var mortgage = {
 		' </soap:Body>' + 
 		'</soap:Envelope>'
 		
+		
 		var url=this.SOAPUrl;
+		//Webcall paramters.
 		var opt={
 			url : url,
 			method : "POST",
@@ -31,8 +37,14 @@ var mortgage = {
 			body:xmlContent,
 			period : 3600
 		 };
+		 
+		 //Feedhenry Web Call
 		var res= $fh.web(opt);
+		
+		// getSOAPElement will return an xml object that exists in SOAP response
 		var xmlData=getSOAPElement("GetMortgagePaymentResult",res.body);
+		
+		// construct final returned JSON object.
 		var rtnObj={
 			MonthlyPrincipalAndInterest:xmlData.MonthlyPrincipalAndInterest.toString(),
 			MonthlyTax:xmlData.MonthlyTax.toString(),
@@ -45,4 +57,4 @@ var mortgage = {
 		
 		
 	}
-}
+};
