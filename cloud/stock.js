a6b7723b0edd58e71f751a233519bb254cb5fa2d
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-22 12:26:48 +0000

    update

--- cloud/stock.js
+++ cloud/stock.js
@@ -22,12 +22,11 @@ var stock = {
 			period:3600
 		});
 		var stockSymbol = this.processSymbolRes(symbolRes);
-		return {sb:stockSymbol};
 		var soapEnvolope='<?xml version="1.0" encoding="utf-8"?>'+
 							+'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
 							+  '<soap:Body>'
 							+    '<GetQuote xmlns="http://www.webserviceX.NET/">'
-							+      '<symbol>string</symbol>'
+							+      '<symbol>'+stockSymbol+'</symbol>'
 							+    '</GetQuote>'
 							+  '</soap:Body>'
 							+'</soap:Envelope>';
