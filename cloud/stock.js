3a3e3f69f82737faef6e20da6f1fbbf38d516f59
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-22 12:24:13 +0000

    update

--- cloud/stock.js
+++ cloud/stock.js
@@ -21,7 +21,6 @@ var stock = {
 			charset:"UTF-8",
 			period:3600
 		});
-		return symbolRes;
 		var stockSymbol = this.processSymbolRes(symbolRes);
 		var soapEnvolope='<?xml version="1.0" encoding="utf-8"?>'+
 							+'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
