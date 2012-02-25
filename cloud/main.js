24c08a3ee6d9d1d6cd539a6d25f4faf250ab1671
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-25 22:01:32 +0000

    update

--- cloud/main.js
+++ cloud/main.js
@@ -2,41 +2,42 @@
  * main entry of cloud side
  */
 
-
 /**
  * Create a new Request ticket in Zendesk with requester
  */
-function newRequest(param){
-	return zendesk.newRequest(param.subject,param.des,param.email);
+function newRequest(param) {
+	return zendesk.newRequest(param.subject, param.des, param.email);
 }
 
 /**
  * Retrieve all request tickets
  */
-function listUserRequests(param){
+function listUserRequests(param) {
 	return zendesk.listUserRequests(param.email);
 }
 
 /**
  * Calculate mortgage with user input
  */
-function getMortgage(param){
-	return mortgage.getMortgage(param.years,param.interest,param.loanAmount,param.tax,param.insurance);
+function getMortgage(param) {
+	return mortgage.getMortgage(param.years, param.interest, param.loanAmount, param.tax, param.insurance);
 }
 
 /**
  * Get stock symbol and detailed information by company name
  */
-function getStockInfo(param){
+function getStockInfo(param) {
 	return stock.getStockInfo(param.name);
 }
 
-exports={
-	newRequest:newRequest,
-	listUserRequests:listUserRequests,
-	getMortgage:getMortgage,
-	getStockInfo:getStockInfo,
-	test:function(){
-		return {aa:"helo"};
-	}
+exports = {
+	newRequest : newRequest,
+	listUserRequests : listUserRequests,
+	getMortgage : getMortgage,
+	getStockInfo : getStockInfo
+}
+exports.test = function() {
+	return {
+		aa : "helo"
+	};
 }
