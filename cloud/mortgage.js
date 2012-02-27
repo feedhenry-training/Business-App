7aa147bad4c0e83205a7b17c4f59f02b8e23791c
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-27 00:23:41 +0000

    libxmljs

--- cloud/mortgage.js
+++ cloud/mortgage.js
@@ -15,7 +15,7 @@ var mortgage = {
 		 */
 		var xmlContent = '<?xml version="1.0" encoding="utf-8"?>' + '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' + ' <soap:Body>' + '   <GetMortgagePayment xmlns="http://www.webserviceX.NET/">' + '   <Years>' + years + '</Years>' + '   <Interest>' + interest + '</Interest>' + '   <LoanAmount>' + loanAmount + '</LoanAmount>' + '   <AnnualTax>' + tax + '</AnnualTax>' + '   <AnnualInsurance>' + insurance + '</AnnualInsurance>' + '   </GetMortgagePayment>' + ' </soap:Body>' + '</soap:Envelope>'
 
-		var url = this.SOAPUrl;
+		var url = mortgage.SOAPUrl;
 		//Webcall paramters.
 		var opt = {
 			url : url,
