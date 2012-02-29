ad1ac4e6d58e09cdd3a8a0182a95d8e610ea4878
Keyang Xiang <kxiang@Keyangs-MacBook-Pro.local>
2012-02-29 15:50:02 +0000

    update zendesk test

--- cloud/zendesk.js
+++ cloud/zendesk.js
@@ -115,9 +115,12 @@ var zendesk = {
 		}
 		var encodedAuth = (new Buffer(auth)).toString("base64");
 		opt.headers["Authorization"] = "Basic " + encodedAuth;
-		var request = require("request");
+		//var request = require("request");
 		// log(opt);
-		request.cookie("");
+		//request.cookie("");
+		$fh.web(opt,function(err,res){
+			cb(null,{"ab":"ab"});
+		});
 		request(opt, function(err, response, body) {
 			// just apply the results object to the data we send back.
 			cb(null, {
@@ -131,3 +134,4 @@ var zendesk = {
 
 exports.newRequest = zendesk.newRequest;
 exports.listUserRequests = zendesk.listUserRequests;
+
